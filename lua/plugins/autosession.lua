9e98464370d0da35490bdd3edb6cc26283b4df54
Manne <mannebusk@gmail.com>
2024-04-20 09:52:51 +0200

    Formating

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -1,11 +1,11 @@
 require("auto-session").setup {
-    log_level = "error",
+  log_level = "error",
 
-    cwd_change_handling = {
-        restore_upcoming_session = true,
-        pre_cwd_changed_hook = nil,
-        post_cwd_changed_hook = function()
-            require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
-        end,
-    },
+  cwd_change_handling = {
+    restore_upcoming_session = true,
+    pre_cwd_changed_hook = nil,
+    post_cwd_changed_hook = function()
+      require("lualine").refresh() -- refresh lualine so the new session name is displayed in the status bar
+    end,
+  },
 }
