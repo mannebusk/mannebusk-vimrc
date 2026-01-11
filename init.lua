6aa63b5c4868bbe806dff78aeb0da8ecf4642bac
Manne <mannebusk@gmail.com>
2026-01-11 21:28:56 +0100

    Add telescope

--- init.lua
+++ init.lua
@@ -86,7 +86,10 @@ vim.pack.add({
     opts_extend = { "sources.default" }
   },
 
-  { src = 'https://github.com/terrortylor/nvim-comment' }
+  { src = 'https://github.com/terrortylor/nvim-comment' },
+
+  { src = 'https://github.com/nvim-lua/plenary.nvim' },
+  { src = 'https://github.com/nvim-telescope/telescope.nvim' }
 })
 
 --
@@ -143,6 +146,18 @@ require('blink.cmp').setup({
 -- Nvim comment
 --
 require('nvim_comment').setup()
+
+
+--
+-- Telescope
+--
+local builtin = require('telescope.builtin')
+
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 
 --
